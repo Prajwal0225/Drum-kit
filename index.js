025d9490998f5ceb46fbc588f5baace3af7bcc5a
Prajwal0225 <103810119+Prajwal0225@users.noreply.github.com>
2022-07-16 12:59:28 +0530

    Add files via upload
    Drup-kit

---
+++ index.js
@@ -0,0 +1,66 @@
+var l = document.querySelectorAll('.drum').length;
+
+for (var i = 0; i < l; i++)
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var btnclick = this.innerHTML;
+
+    PlyingSound(btnclick);
+    btnAnnimation(btnclick);
+  })
+
+function PlyingSound(input){
+  //switch statement
+  switch (input) {
+    case 'w':
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case 'a':
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case 's':
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case 'd':
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case 'j':
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case 'k':
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case 'l':
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default:  console.log(btnclick);
+
+  }
+
+}
+  document.addEventListener("keydown", function(e){
+  //  alert("key from keyboard got clicked");
+   PlyingSound(e.key);
+   btnAnnimation(e.key);
+  })
+
+
+function btnAnnimation(btn){
+    var ee = document.querySelector("." + btn);
+    ee.classList.add("pressed");
+
+    setTimeout(function(){
+      ee.classList.remove("pressed");
+    },100)
+
+  }
+
+
+// var audio = new Audio("sounds/crash.mp3");
+// audio.play();
